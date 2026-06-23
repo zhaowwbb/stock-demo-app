be580779e929550323fcbaeeca82c266384b656b
DESKTOP-5L5DCJB\Rick <zhaowwbb@gmail.com>
2026-06-23 00:55:53 -0400

    sql to create stock data

---
+++ database/stocks_data.sql
@@ -0,0 +1,54 @@
+INSERT INTO stocks (symbol, company_name, price, score, updated_time) VALUES
+-- Technology & Semiconductors
+('GOOGL', 'Alphabet Inc.', 349.56, 88.50, NOW()),
+('AMD', 'Advanced Micro Devices, Inc.', 164.20, 82.10, NOW()),
+('AVGO', 'Broadcom Inc.', 1720.45, 91.30, NOW()),
+('QCOM', 'QUALCOMM Incorporated', 205.10, 85.40, NOW()),
+('CRM', 'Salesforce, Inc.', 242.30, 79.80, NOW()),
+('ADBE', 'Adobe Inc.', 485.60, 81.50, NOW()),
+('CSCO', 'Cisco Systems, Inc.', 46.80, 72.40, NOW()),
+('SMCI', 'Super Micro Computer, Inc.', 785.00, 89.90, NOW()),
+('PLTR', 'Palantir Technologies Inc.', 25.40, 87.20, NOW()),
+('PANW', 'Palo Alto Networks, Inc.', 312.15, 84.60, NOW()),
+('UBER', 'Uber Technologies, Inc.', 68.90, 83.10, NOW()),
+
+-- Financials & Payments
+('BRK-B', 'Berkshire Hathaway Inc.', 488.69, 93.00, NOW()),
+('JPM', 'JPMorgan Chase & Co.', 198.50, 92.50, NOW()),
+('BAC', 'Bank of America Corporation', 38.20, 78.10, NOW()),
+('WFC', 'Wells Fargo & Company', 56.40, 76.80, NOW()),
+('MS', 'Morgan Stanley', 94.15, 80.50, NOW()),
+('GS', 'The Goldman Sachs Group, Inc.', 452.80, 83.40, NOW()),
+('V', 'Visa Inc.', 272.30, 89.50, NOW()),
+('MA', 'Mastercard Incorporated', 445.10, 90.20, NOW()),
+('AXP', 'American Express Company', 231.60, 85.10, NOW()),
+('PYPL', 'PayPal Holdings, Inc.', 62.45, 68.90, NOW()),
+
+-- Consumer Staples & Discretionary
+('WMT', 'Walmart Inc.', 67.20, 89.00, NOW()),
+('HD', 'The Home Depot, Inc.', 335.80, 84.20, NOW()),
+('COST', 'Costco Wholesale Corporation', 842.50, 92.10, NOW()),
+('TGT', 'Target Corporation', 142.10, 74.50, NOW()),
+('NKE', 'NIKE, Inc.', 92.40, 76.20, NOW()),
+('SBUX', 'Starbucks Corporation', 79.15, 71.80, NOW()),
+('KO', 'The Coca-Cola Company', 62.50, 86.50, NOW()),
+('PEP', 'PepsiCo, Inc.', 166.80, 85.40, NOW()),
+('DIS', 'The Walt Disney Company', 101.30, 77.30, NOW()),
+('CMG', 'Chipotle Mexican Grill, Inc.', 3150.00, 88.90, NOW()),
+
+-- Healthcare & Pharmaceuticals
+('LLY', 'Eli Lilly and Company', 1102.08, 96.40, NOW()),
+('UNH', 'UnitedHealth Group Incorporated', 492.50, 88.20, NOW()),
+('JNJ', 'Johnson & Johnson', 146.30, 81.10, NOW()),
+('PFE', 'Pfizer Inc.', 27.90, 65.40, NOW()),
+('MRK', 'Merck & Co., Inc.', 124.50, 83.70, NOW()),
+('ABBV', 'AbbVie Inc.', 168.20, 85.90, NOW()),
+('ISRG', 'Intuitive Surgical, Inc.', 432.10, 89.20, NOW()),
+
+-- Energy & Industrials
+('XOM', 'Exxon Mobil Corporation', 138.47, 84.00, NOW()),
+('CVX', 'Chevron Corporation', 154.20, 81.30, NOW()),
+('CAT', 'Caterpillar Inc.', 328.60, 86.10, NOW()),
+('GE', 'General Electric Company', 162.40, 84.50, NOW()),
+('F', 'Ford Motor Company', 11.85, 69.20, NOW()),
+('GM', 'General Motors Company', 45.30, 73.60, NOW());
